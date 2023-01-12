65085da6622541200610940bbc1e7e51ca2b3587
ALTOM-PC63\raluca.popa <popa.raluca.oana@gmail.com>
2023-01-12 18:55:45 +0200

    book cart api : more tests for other GET requests

--- BookCartAPI/locustfile.py
+++ BookCartAPI/locustfile.py
@@ -3,12 +3,25 @@ from locust import HttpUser, SequentialTaskSet, task
 HEADERS = {
     "Content-Type": "application/json",
 }
+BOOK_ID = '32'
 
 class TaskSet(SequentialTaskSet):
 
     @task
     def list_all_books(self):
-        self.client.get(url=f"/Book", headers=HEADERS)
+        self.client.get(url="/Book", headers=HEADERS)
+
+    @task
+    def read_one_book(self):
+        self.client.get(url=f"/Book/{BOOK_ID}", headers=HEADERS)
+
+    @task
+    def list_all_categories(self):
+        self.client.get(url="/Book/GetCategoriesList", headers=HEADERS)
+
+    @task
+    def list_5_similar_books(self):
+        self.client.get(url=f"/Book/GetSimilarBooks/{BOOK_ID}", headers=HEADERS)
 
 class AllOperationsUser(HttpUser):
     """Locust class that sets the Tasks Set(User Behaviour)
