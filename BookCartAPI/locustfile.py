48e25b9cb3f52f2c1f08a4a8311fd89dda9409cd
ALTOM-PC63\raluca.popa <popa.raluca.oana@gmail.com>
2023-01-25 11:30:35 +0200

    add more GET requests to BookCart locust file

--- BookCartAPI/locustfile.py
+++ BookCartAPI/locustfile.py
@@ -4,6 +4,8 @@ HEADERS = {
     "Content-Type": "application/json",
 }
 BOOK_ID = '32'
+USER_NAME = 'ralutest'
+USER_ID = '1920'
 
 class TaskSet(SequentialTaskSet):
 
@@ -22,6 +24,22 @@ class TaskSet(SequentialTaskSet):
     @task
     def list_5_similar_books(self):
         self.client.get(url=f"/Book/GetSimilarBooks/{BOOK_ID}", headers=HEADERS)
+
+    @task
+    def check_username_availability(self):
+        self.client.get(url=f"/User/validateUserName/{USER_NAME}", headers=HEADERS)
+
+    @task
+    def read_count_of_items(self):
+        self.client.get(url=f"/User/{USER_ID}", headers=HEADERS)
+
+    @task
+    def list_items_user_wishlist(self):
+        self.client.get(url=f"/Wishlist/{USER_ID}", headers=HEADERS)
+
+    @task
+    def list_items_user_shopping_cart(self):
+        self.client.get(url=f"/ShoppingCart/{USER_ID}", headers=HEADERS)
 
 class AllOperationsUser(HttpUser):
     """Locust class that sets the Tasks Set(User Behaviour)
