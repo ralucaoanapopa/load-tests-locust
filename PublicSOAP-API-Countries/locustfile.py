af82179bbc2d4a5f7e893602737d36810c51feee
ALTOM-PC63\raluca.popa <popa.raluca.oana@gmail.com>
2023-01-27 15:11:28 +0200

    get capital for one country and use name for def

--- PublicSOAP-API-Countries/locustfile.py
+++ PublicSOAP-API-Countries/locustfile.py
@@ -14,18 +14,41 @@ XML_BODY = """<?xml version="1.0" encoding="utf-8"?>
 </soap12:Envelope>
 """
 
+XML_COUNTRY_BODY = """<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+  <soap:Body>
+    <CapitalCity xmlns="http://www.oorsprong.org/websamples.countryinfo">
+      <sCountryISOCode>AF</sCountryISOCode>
+    </CapitalCity>
+  </soap:Body>
+</soap:Envelope>
+"""
+
 class TaskSet(SequentialTaskSet):
 
     @task
     def list_of_countries_by_name(self):
-        response = self.client.post(url = "",
-                        data=XML_BODY,
-                        headers = HEADERS)
-
+      with self.client.post(url = "",
+                      data = XML_BODY,
+                      headers = HEADERS,
+                      name = "list_of_countries_by_name") as response:
         json_resp = xmltodict.parse(response.text)
         list_countries = json_resp['soap:Envelope']['soap:Body']['m:ListOfCountryNamesByNameResponse']['m:ListOfCountryNamesByNameResult']['m:tCountryCodeAndName']
 
+        if response.status_code != 200:
+          response.failure("Got wrong status code")
+          
         assert len(list_countries) == 246
+
+    @task
+    def get_capital_city_for_country_by_iso_code(self):
+      with self.client.post(url = "",
+                      data = XML_COUNTRY_BODY,
+                      headers = HEADERS,
+                      name = "get_capital_city_for_country_by_iso_code") as response:
+        json_resp = xmltodict.parse(response.text)
+        capital = json_resp['soap:Envelope']['soap:Body']['m:CapitalCityResponse']['m:CapitalCityResult']
+        assert capital == 'Kabul'
 
 class AllOperationsUser(HttpUser):
     """Locust class that sets the Tasks Set(User Behaviour)
